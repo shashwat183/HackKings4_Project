3054400030a1a67baaf79068f1ea915001035246
Shashwat pragya <shashwat183@gmail.com>
2017-11-26 12:55:27 +0000

    was

--- recieverscript.py
+++ recieverscript.py
@@ -2,8 +2,8 @@ import boto3
 import os
 import time
 
-access_key = "AKIAITZYIKJ6R2NVI27Q"
-access_secret = "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"
+access_key = "AKIAJERJ5MEB3ANEJPJA"
+access_secret = "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"
 region = "us-east-1"
 queue_url = "https://sqs.us-east-1.amazonaws.com/593887092556/alexascript"
 
