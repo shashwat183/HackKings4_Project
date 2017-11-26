e056910da28d58bbb6178cd14268e5e64409e3ac
Shashwat pragya <shashwat183@gmail.com>
2017-11-26 12:43:21 +0000

    adsfs

---
+++ recieverscript.py
@@ -0,0 +1,37 @@
+import boto3
+import os
+import time
+
+access_key = "AKIAIE2SNPHYVFYORXZA"
+access_secret = "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"
+region = "us-east-1"
+queue_url = "https://sqs.us-east-1.amazonaws.com/593887092556/alexascript"
+
+def pop_message(client, url):
+    response = client.receive_message(QueueUrl = url, MaxNumberOfMessages = 10)
+
+    #last message posted becomes messages
+    message = response['Messages'][0]['Body']
+    receipt = response['Messages'][0]['ReceiptHandle']
+    client.delete_message(QueueUrl = url, ReceiptHandle = receipt)
+    return message
+
+client = boto3.client('sqs', aws_access_key_id = access_key, aws_secret_access_key = access_secret, region_name = region)
+
+waittime = 20
+client.set_queue_attributes(QueueUrl = queue_url, Attributes = {'ReceiveMessageWaitTimeSeconds': str(waittime)})
+
+time_start = time.time()
+while (time.time() - time_start < 60):
+        print("Checking...")
+        try:
+                message = pop_message(client, queue_url)
+                print(message)
+                if message == "on":
+                        os.system("~/tvon.sh")
+                elif message == "off":
+                        os.system("~/tvoff.sh")
+                elif message == "recognise":
+                        os.system("./shazamscript.sh")
+        except:
+                pass
