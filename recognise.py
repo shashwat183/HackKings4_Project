d2ff503895b38572db44448c9b6d96cc7f283d2a
SHashwat Pragya <shashwat183@gmail.com>
2017-11-26 12:40:14 +0000

    dd

--- recognise.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-import sys
-import json
-
-command = curl -X POST -H "X-Shazam-Api-Key: 301e9c4f-6cb2-4128-915d-10eadccf7e9d" -H "Content-Type: application/octet-stream" --data-binary "@hipe.wav" "https://hackathon.shazam.com/partner/recognise"
-os.system('./shazamscript.sh')
-#print ('the song is ' + json_data["title"] + 'by ' + json_data["artist"]);
-#os.system('rm','result.json');
