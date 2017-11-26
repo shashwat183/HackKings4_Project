7c61a4d9134f51ea3466937f86c8baec257351ab
SHashwat Pragya <shashwat183@gmail.com>
2017-11-26 03:38:20 +0000

    added shazam shell script

---
+++ shazamscript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm hipe.wav
+timeout 4 rec hipe.wav
+curl -X POST -H "X-Shazam-Api-Key: 301e9c4f-6cb2-4128-915d-10eadccf7e9d" -H "Content-Type: application/octet-stream" --data-binary "@hipe.wav" "https://hackathon.shazam.com/partner/recognise"
+
